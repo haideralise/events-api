4a1274f679d1aee02b60ec98df161362a6bf7582
Haider Ali <se.haiderali@gmail.com>
2019-08-25 16:46:40 +0500

    added relations and guraded fields

--- app/Event.php
+++ app/Event.php
@@ -2,9 +2,29 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $guarded = [
+        'id',
+        'creator_id',
+        'status_id'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id', 'id');
+    }
 }
